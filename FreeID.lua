a3307527fc56f632bc0c7b50efcc9790e2a64020
nothing <minudoindo@gmail.com>
2026-03-13 23:42:04 -0300

    Update FreeID.lua

--- FreeID.lua
+++ FreeID.lua
@@ -5,7 +5,7 @@ GalaxHubGames = {
     }, -- Precisa de virgula viu
     {
         ids = { "14890802310", "74747090658891", "77923379150463" },
-        script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/WhyMayko/Lineage/refs/heads/main/main.lua"))()]]
+        script = [[loadstring(game:HttpGet(""))()]]
     }
        -- Adicione mais jogos aqui!
 }
