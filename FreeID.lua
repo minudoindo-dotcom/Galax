73ac6d0d3c6a858567833700cc9cbe40d9e166e2
nothing <minudoindo@gmail.com>
2026-03-13 23:22:46 -0300

    Create FreeID.lua

---
+++ FreeID.lua
@@ -0,0 +1,11 @@
+GalaxHubGames = {
+    {
+        ids = { "131334102981924" },
+        script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/WhyMayko/Skid-Galaxy/refs/heads/main/main.lua"))()]]
+    }, -- Precisa de virgula viu
+    {
+        ids = { "14890802310", "74747090658891", "77923379150463" },
+        script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/WhyMayko/Lineage/refs/heads/main/main.lua"))()]]
+    }
+       -- Adicione mais jogos aqui!
+}
