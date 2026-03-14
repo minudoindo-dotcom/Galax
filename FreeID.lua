d937305fca6da8f58d05536c87eb902ca150e479
nothing <minudoindo@gmail.com>
2026-03-13 23:40:06 -0300

    Update FreeID.lua

--- FreeID.lua
+++ FreeID.lua
@@ -1,7 +1,7 @@
 GalaxHubGames = {
     {
         ids = { "131334102981924" },
-        script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/WhyMayko/Skid-Galaxy/refs/heads/main/main.lua"))()]]
+        script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/minudoindo-dotcom/Galax/refs/heads/main/Scripts/Free/Jujutsu.lua"))()]]
     }, -- Precisa de virgula viu
     {
         ids = { "14890802310", "74747090658891", "77923379150463" },
