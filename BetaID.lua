2f2d713a74303abea56849017a8fab9bb82f69b3
WhyMayko <128808705+WhyMayko@users.noreply.github.com>
2026-03-19 16:36:09 -0300

    Update BetaID.lua

--- BetaID.lua
+++ BetaID.lua
@@ -1,11 +1,10 @@
 GalaxHubGames = {
     {
         ids = { "9391468976", "17016840407" },
-        script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/minudoindo-dotcom/Galax/refs/heads/main/Scripts/Beta/Jujutsu.lua"))()]]
-    }, -- Precisa de virgula viu
+        script = "https://raw.githubusercontent.com/minudoindo-dotcom/Galax/refs/heads/main/Scripts/Beta/Jujutsu.lua"
+    },
     {
         ids = { "14890802310", "74747090658891", "77923379150463" },
-        script = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/minudoindo-dotcom/Galax/refs/heads/main/Scripts/Free/BizarreLineage.lua"))()]]
+        script = "https://raw.githubusercontent.com/minudoindo-dotcom/Galax/refs/heads/main/Scripts/Free/BizarreLineage.lua"
     }
-       -- Adicione mais jogos aqui!
 }
